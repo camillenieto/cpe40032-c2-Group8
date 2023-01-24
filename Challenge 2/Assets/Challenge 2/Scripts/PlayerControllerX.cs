3640de372ab593a917b3b3c187454128f8b3441b
Camillle Nieto <nietocamille06@gmail.com>
2023-01-24 15:06:37 +0800

    challenge 2
    .gitignore
    .gitattributes

---
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogDelay = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Every frame the speed of the dog will slowdown at the speed of -1.
+        dogDelay -= 0.1f;
+        // On spacebar press
+        if (Input.GetKeyDown(KeyCode.Space) && dogDelay <= 0.0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            //Return speed to 20.
+            dogDelay = 12.0f;
+        }
+    }
+}
+
